7fd3dd09ff5008dfa31934c63d387336e3876f28
Salil <swadnerkar@study.com>
2016-05-05 08:07:08 -0700

    assignment 3 - initial

--- Assignment1/src/main/java/HashSubstring.java
+++ Assignment1/src/main/java/HashSubstring.java
@@ -1,63 +1,67 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.StringTokenizer;
 
 public class HashSubstring {
 
-    private static FastScanner in;
-    private static PrintWriter out;
-
+    static final private int prime = 1000000007;
+    
     public static void main(String[] args) throws IOException {
-        in = new FastScanner();
-        out = new PrintWriter(new BufferedOutputStream(System.out));
-        printOccurrences(getOccurrences(readInput()));
-        out.close();
-    }
-
-    private static Data readInput() throws IOException {
+        FastScanner in = new FastScanner();
         String pattern = in.next();
         String text = in.next();
-        return new Data(pattern, text);
-    }
-
-    private static void printOccurrences(List<Integer> ans) throws IOException {
-        for (Integer cur : ans) {
-            out.print(cur);
-            out.print(" ");
+        List<Integer> ans = getOccurrences(pattern, text);
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out))) {
+            for (Integer cur : ans) {
+                out.print(cur);
+                out.print(" ");
+            }
         }
     }
 
-    private static List<Integer> getOccurrences(Data input) {
-        String s = input.pattern, t = input.text;
-        int m = s.length(), n = t.length();
-        List<Integer> occurrences = new ArrayList<Integer>();
+    private static int hashFunc(String s, int multiplier) {
+        long hash = 0;
+        for (int i = s.length() - 1; i >= 0; --i)
+            hash = (hash * multiplier + s.charAt(i)) % prime;
+        return (int)hash;
+    }
+
+    private static List<Integer> getOccurrences(String p, String t) {
+        // Generate random multiplier between 1 and prime-1
+        int multiplier = new Random().nextInt(prime-1) + 1;
+        int m = p.length();
+        int n = t.length();
+        List<Integer> occurrences = new ArrayList<>();
+        int pHash = hashFunc(p, multiplier);
+        int[] h = precomputeHashes(t, m, multiplier);
         for (int i = 0; i + m <= n; ++i) {
-	    boolean equal = true;
-	    for (int j = 0; j < m; ++j) {
-		if (s.charAt(j) != t.charAt(i + j)) {
-		     equal = false;
- 		    break;
-		}
-	    }
-            if (equal)
+            if (pHash != h[i])
+                continue;
+            if (t.substring(i, i+m).equals(p))
                 occurrences.add(i);
 	}
         return occurrences;
     }
 
-    static class Data {
-        String pattern;
-        String text;
-        public Data(String pattern, String text) {
-            this.pattern = pattern;
-            this.text = text;
+    private static int[] precomputeHashes(String t, int m, int multiplier) {
+        int n = t.length();
+        int[] h = new int[n - m  + 1];
+        String last = t.substring(n - m);
+        h[n - m] = hashFunc(last, multiplier);
+        int y = 1;
+        for (int i = 1; i <= m; ++i) {
+            y = (y * multiplier) % prime;
         }
+        for (int i = n - m - 1; i >= 0; --i) {
+            h[i] = (multiplier * h[i + 1] + t.charAt(i) - y * t.charAt(i + m)) % prime;
+        }
+        return h;
     }
-
+    
     static class FastScanner {
-        private BufferedReader reader;
+        private final BufferedReader reader;
         private StringTokenizer tokenizer;
 
         public FastScanner() {
